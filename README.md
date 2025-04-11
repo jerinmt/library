1b9c92c51b8424b8f7429a0ed1b8cf8cd963f84c
jerinmt <jerinmt379@gmail.com>
2025-04-11 12:32:41 +0530

    Completed the project

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Going to make a simple page with a header
 9:50 pm 
 Completed the html and css portions. Started the javascript portion.
 Pushed to git.
+
+11 April 2025
+Tweaking a little in css, to bring the add new book button to main from header. Then work on add button scripting, then read button. And finally the remove book option.
+
+Finished the project at 12:30 pm and pushed to git.
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const myLibrary = [];
+const myCollection = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
     if(!new.target) {
         throw Error("Use the 'new' operator to call the constructor");   
     }
@@ -8,30 +8,87 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
     this.isRead = false;
+    this.id = id;
     this.info = function() {
         let reply;
         if(this.isRead) {
-            reply = `${this.title} by ${this.author}, ${this.pages}. Not read yet`;    
+            reply = `${this.title} by ${this.author}, ${this.pages} pages long. Already read`;
         }
         else {
-            reply = `${this.title} by ${this.author}, ${this.pages}. Already read`;
+            reply = `${this.title} by ${this.author}, ${this.pages} pages long. Not read yet`;
         }
         return reply;
     }
 }
 Book.prototype.haveRead = function() {
-    this.isRead = !this.isRead;
+    this.isRead = true;
 }
 
 function addBook() {
-    let bookName, authorName, pageCount;
-    //take parameters
-    let bookObject = new Book(bookName, authorName, pageCount);
-    myLibrary.push(bookObject);
+    let bookName, authorName, pageCount, id;
+    bookName = document.querySelector("#newTitle").value;
+    authorName = document.querySelector("#newAuthor").value;
+    pageCount = document.querySelector("#newPages").value;
+    id = crypto.randomUUID();
+    let bookObject = new Book(bookName, authorName, pageCount, id);
+    myCollection.push(bookObject);
+    populateLibrary();
 }
 
-document.querySelectorAll("#addButton").addEventlistener('click', ()=> {
-    let modalBox = document.querySelector("dialog");
-    modalBox.show();
-    modalBox.
+
+document.querySelector(".addButton").addEventListener('click', ()=> {
+    document.querySelector("dialog").showModal();
 });
+document.querySelector("#updateBook").addEventListener('click', ()=> {
+    addBook();
+    document.querySelector("dialog").close();
+});
+
+function populateLibrary() {
+    let shelf = document.querySelector("main > div");
+    let book, closeButton, title, image, info, readButton;
+    book = document.createElement("div");
+    book.classList.add("cards");
+    book.dataset.uniqueID = myCollection[myCollection.length-1].id;
+    shelf.appendChild(book);
+    closeButton = document.createElement("button");
+    closeButton.type = "button";
+    closeButton.classList.add("closeButton");
+    closeButton.textContent = 'X';
+    closeButton.addEventListener('click', (Event)=>{
+        let parent = Event.target.parentNode;
+        let id = parent.dataset.uniqueID;    
+        parent.remove();
+        for(let i=0;i<myCollection.length;i++) {
+            if(myCollection[i].id==id) {
+                myCollection.splice(i,1);
+            }
+        }
+    });
+    book.appendChild(closeButton);
+    title = document.createElement("h3");
+    title.textContent = myCollection[myCollection.length-1].title;
+    book.appendChild(title);
+    image = document.createElement("img");
+    image.src = "./assets/view-dashboard.svg";
+    book.appendChild(image);
+    info = document.createElement("p");
+    info.textContent = myCollection[myCollection.length-1].info();
+    book.appendChild(info);
+    readButton = document.createElement("button");
+    readButton.type = "button";
+    readButton.classList.add("haveRead");
+    readButton.textContent = 'Read';
+    readButton.addEventListener('click', (Event)=>{
+        let parent = Event.target.parentNode;
+        let id = parent.dataset.uniqueID;
+        for(let i=0;i<myCollection.length;i++) {
+            if(myCollection[i].id==id) {
+                myCollection[i].haveRead();
+                Event.target.previousSibling.textContent = myCollection[i].info();
+                Event.target.remove();
+            }
+        }        
+    });
+    book.appendChild(readButton);
+}
